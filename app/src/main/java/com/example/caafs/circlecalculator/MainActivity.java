7eb4efa3b91333015b8b399f151a1ef0c8134d71
Cassandra Afseth <cafseth2@Illinois.edu>
2018-05-01 01:17:53 -0500

    format

--- app/src/main/java/com/example/caafs/circlecalculator/MainActivity.java
+++ app/src/main/java/com/example/caafs/circlecalculator/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.caafs.circlecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView results;
+    private Button buttoncalculate;
+    private EditText radius;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        results = (TextView) findViewById(R.id.results);
+        buttoncalculate = (Button)findViewById(R.id.calcbutton);
+        radius = (EditText)findViewById(R.id.radius);
+        buttoncalculate.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        double rad = Double.parseDouble(radius.getText().toString());
+        CircleClass circleOne = new CircleClass(rad);
+        results.setText("Area: " + circleOne.getArea() + "\nCircumference: " + circleOne.getCircumference());
     }
 }
